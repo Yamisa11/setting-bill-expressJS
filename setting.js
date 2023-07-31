7be29b42128c68e450c44057a2e78dd1d3c262eb
Yamisa11 <majijayamisa@gmail.com>
2023-07-31 12:12:13 +0200

    update

--- setting.js
+++ setting.js
@@ -93,8 +93,8 @@ export default function SettingsBill() {
     }
 
     function totals() {
-        let smsTotal = getTotal('sms')
-        let callTotal = getTotal('call')
+        let smsTotal = getTotal('sms').toFixed(2)
+        let callTotal = getTotal('call').toFixed(2)
         return {
             smsTotal,
             callTotal,
